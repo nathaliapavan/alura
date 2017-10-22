2011416538f1431c6c9b71ab24ee4200521155b8
nathaliapavan <nanahpavan@gmail.com>
2017-10-21 23:46:13 -0200

    Aula 05 - Melhorando a listagem de produtos

--- PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/adiciona-produto.php
+++ PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/adiciona-produto.php
@@ -1,12 +1,7 @@
-<?php include("cabecalho.php"); ?>
-<?php include("conecta.php"); ?>
-
-<?php
-
-function insereProduto($conexao, $nome, $preco) {
-	$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
-	return mysqli_query($conexao, $query);
-}
+<?php 
+include("cabecalho.php");
+include("conecta.php"); 
+include("banco-produto.php"); 
 
 $nome = $_GET["nome"];
 $preco = $_GET["preco"];
