e603c55d143a4a6ae8626c2402a1fbb6baf7dab8
nathaliapavan <nanahpavan@gmail.com>
2017-10-22 01:22:53 -0200

    Aula 07 - Diferença entre GET e POST

--- PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/banco-produto.php
+++ PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/banco-produto.php
@@ -1,7 +1,7 @@
 <?php
 
-function insereProduto($conexao, $nome, $preco) {
-	$query = "INSERT INTO produtos (nome, preco) VALUES ('{$nome}', {$preco})";
+function insereProduto($conexao, $nome, $preco, $descricao) {
+	$query = "INSERT INTO produtos (nome, preco, descricao) VALUES ('{$nome}', {$preco}, '{$descricao}')";
 	return mysqli_query($conexao, $query);
 }
 
--- PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/database.sql
+++ PHP e MySQL I - Fundamentos para criar um sistema na Web/loja/database.sql
@@ -1,4 +1,8 @@
-CREATE TABLEA produtos (
-
-
+CREATE TABLE produtos (
+	id INTEGER PRIMARY KEY AUTO INCREMENT,
+	nome VARCHAR(255),
+	preco DECIMAL(10,2)
 );
+
+ALTER TABLE produtos ADD COLUMN descricao TEXT;
+UPDATE produtos SET descricao = 'Descricao deste produto';
