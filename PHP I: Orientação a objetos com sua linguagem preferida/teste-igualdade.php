552e8f2b20ce6aae2a61f3c3d96c500002097798
nathaliapavan <nanahpavan@gmail.com>
2017-12-02 18:52:16 -0200

    Aula 05 - Comparando objetos

---
+++ PHP I: Orientação a objetos com sua linguagem preferida/teste-igualdade.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once("class/Produto.php");
+
+$produto = new Produto();
+$produto->setPreco(59.9);
+//$produto->setNome('teste');
+
+$outroProduto = new Produto();
+$outroProduto->setPreco(59.9);
+
+if ($produto == $outroProduto) {
+	echo 'Objetos iguais';
+}else {
+	echo 'Objetos diferentes';
+}
+
+/*
+ * Para comparar duas instancias, compara-se com ===
+ * Quando se deseja comparar os atributos compara-se com ==
+ */
+
+?>
